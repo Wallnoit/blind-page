a5f70d243e06b539a3d3b8382706bf522f0359b0
William <wulli.mu28@gmail.com>
2024-01-26 14:12:42 -0500

    ultimos avances

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	theme: {
 		fontFamily: {
 			sans: [ "Old English (LET) W02", "sans-serif"]
@@ -8,5 +8,8 @@ module.exports = {
 		extend: {},
 
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animated'),
+		require('flowbite/plugin')
+	],
 }
