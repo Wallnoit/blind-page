2a4309a0bc5f526056f0e95b1851aa6626aa8215
William Perez <wulli.mu28@gmail.com>
2024-08-31 18:18:49 -0500

    added video in newsSection

--- src/components/InformationCard.tsx
+++ src/components/InformationCard.tsx
@@ -1,23 +1,42 @@
 
-export default function InformationCard({ title, date, content, img }: { title: string, date: string, content: string, img: ImageMetadata }) {
+export default function InformationCard({ title, date, content, link, video }: { title: string, date: string, content: string, link: string, video: boolean }) {
     return (
-    <div className=" flex flex-col items-center gap-4 my-12 ">
+        <div className=" flex flex-col items-center gap-4 my-12 ">
 
-    <img
-  className="max-h-96 object-cover object-center "
-  src={img.src}
-  alt=""
 
-/>
-        <h1 className=" text-3xl">{title}</h1>
-        <p className=" text-lg text-gray-500">{content}</p>
-        
-        
-        <div className="w-full flex justify-end text-gray-500 ">
-            <p>Fecha: {date}</p>
+            {
+                video ?
 
+                    <video
+                        autoPlay
+                        muted
+                        loop
+                        src={link}
+                        className="max-h-96 object-cover object-center bg-black " />
+
+                    :
+
+                    <img
+                        className="max-h-96 object-cover object-center "
+                        src={link}
+                        alt=""
+
+                    />
+
+
+
+            }
+
+
+            <h1 className=" text-3xl">{title}</h1>
+            <p className=" text-lg text-gray-500">{content}</p>
+
+
+            <div className="w-full flex justify-end text-gray-500 ">
+                <p>Fecha: {date}</p>
+
+            </div>
         </div>
-    </div>
     );
 
 }
--- src/data/news.ts
+++ src/data/news.ts
@@ -3,6 +3,8 @@ import noticia2 from "../assets/noticia2.jpg"
 import noticia3 from "../assets/noticia3.jpg"
 import noticia4 from "../assets/noticia4.jpg"
 import noticia5 from "../assets/noticia5.png"
+import camiseta_1 from "../assets/camiseta_1.mp4"
+import camiseta_2 from "../assets/camiseta_2.mp4"
 
 
 
@@ -10,15 +12,33 @@ type News = {
     title: string;
     date: string;
     content: string;
-    img: ImageMetadata;
+    link: string;
+    video: boolean;
 }
 
 export const newsInformation: News[] = [
+
+    {
+        title: "Born to Rap Collection 2024",
+        date: "01/01/2021",
+        content: "Nuevo diseños disponibles",
+        link: camiseta_1,
+        video: true
+    },
+
+    {
+        title: "Ego trip Collection 2024",
+        date: "01/01/2021",
+        content: "Nuevo diseños disponibles",
+        link: camiseta_2,
+        video: true
+    },
     {
         title: "Lorem ipsum dolor sit amet 1",
         date: "01/01/2021",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus.",
-        img: noticia1
+        link: noticia1.src,
+        video: false
 
     },
 
@@ -26,7 +46,8 @@ export const newsInformation: News[] = [
         title: "Lorem ipsum dolor sit amet 2",
         date: "01/01/2021",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus.",
-        img: noticia2
+        link: noticia2.src,
+        video: false
 
     },
 
@@ -34,7 +55,8 @@ export const newsInformation: News[] = [
         title: "Lorem ipsum dolor sit amet 3",
         date: "01/01/2021",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus.",
-        img: noticia3
+        link: noticia3.src,
+        video: false
 
     },
 
@@ -42,16 +64,18 @@ export const newsInformation: News[] = [
         title: "Lorem ipsum dolor sit amet 4",
         date: "01/01/2021",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus.",
-        img: noticia4
+        link: noticia4.src,
+        video: false
     },
 
     {
         title: "Lorem ipsum dolor sit amet 5",
         date: "01/01/2021",
         content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus. Nulla nec purus feugiat, molestie ipsum et, ultricies metus.",
-        img: noticia5
+        link: noticia5.src,
+        video: false
 
     },
 
-   
+
 ]
